0c484f743a38cbdcc77a115d2f24c3e79206f1a0
Dmytro Shevchenko <0911dimon@gmail.com>
2023-08-10 22:43:12 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles/styles.css'
+
+interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
+
+  return (
+    <div>Project start</div>
+  )
+};
+
+export default App;
