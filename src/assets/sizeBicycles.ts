8469cb6d9a21a2527e1e2475d530f7a5b90f7e87
Alexey Sevastynov <alexseva94@gmail.com>
2023-08-15 10:45:31 +0300

    files assets move to file constants.ts

--- src/assets/sizeBicycles.ts
+++
@@ -1,1 +0,0 @@
-export const sizeBicycles = ["XXS", "XS", "S", "M", "L", "XL", "XXL"];
---
+++ src/constants.ts
@@ -0,0 +1,49 @@
+export const brandBicycles = [
+  "Bergamont",
+  "Bianchi",
+  "BMC",
+  "Boardman",
+  "Brompton",
+  "Cannondale",
+  "Cinelli",
+  "Cube",
+  "Giant",
+  "Merida",
+  "Marin",
+  "Specialized",
+  "Scott",
+  "Orbea",
+  "Tifosi",
+  //... can add yet
+];
+
+export const categoryBicycles = [
+  "BMX",
+  "Cyclocross",
+  "Electric",
+  "Folding",
+  "Hybrid",
+  "Jump",
+  "Kids",
+  "MTB",
+  "Road",
+  "Touring",
+  "New",
+  //... can add yet
+];
+
+export const colorBicycles = [
+  "black",
+  "red",
+  "yellow",
+  "orange",
+  "blue",
+  "green",
+  "pink",
+  "gray",
+  "white",
+  "beige",
+  //... can add yet
+];
+
+export const sizeBicycles = ["XXS", "XS", "S", "M", "L", "XL", "XXL"];
