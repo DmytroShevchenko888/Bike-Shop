f0a11e62079ff5c60a361d74a35e4c0e3e6188db
Alexey Sevastynov <alexseva94@gmail.com>
2023-08-15 13:03:00 +0300

    remove comments in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles/styles.css";
-import { useAppDispatch, useAppSelector } from "./redux/hook";
+import { useAppDispatch } from "./redux/hook";
 import { fetchAllBicycles, fetchOneBicycle } from "./redux/thunks";
 
 interface AppProps {}
@@ -8,29 +8,9 @@ interface AppProps {}
 const App: React.FC<AppProps> = () => {
   const dispatch = useAppDispatch();
 
-  // const {
-  //   allbicycles,
-  //   isLoaded: isLoadedBicycles,
-  //   error: errorBicycles,
-  // } = useAppSelector((state) => state.bicycles.bicycles);
-
-  // const {
-  //   currentbicycle,
-  //   isLoaded: isLoadedBicycle,
-  //   error: errorBicycle,
-  // } = useAppSelector((state) => state.bicycles.bicycle);
-
-  // console.log("allbicycles:", allbicycles);
-  // console.log("isLoadedBicycles:", isLoadedBicycles);
-  // console.log("errorBicycles:", errorBicycles);
-
-  // console.log("currentbicycle:", currentbicycle);
-  // console.log("isLoadedBicycle:", isLoadedBicycle);
-  // console.log("errorBicycle:", errorBicycle);
-
   React.useEffect(() => {
-    dispatch(fetchAllBicycles());
-    dispatch(fetchOneBicycle({ id: "64d7c1616da8b8269434d66f" }));
+    dispatch(fetchAllBicycles()); // get array all bicycles
+    dispatch(fetchOneBicycle({ id: "64d7c1616da8b8269434d66f" })); // get object one bicycle
   }, [dispatch]);
 
   return <div>Project start</div>;
