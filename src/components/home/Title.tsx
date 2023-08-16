6395c26c7b981b6b799b46bbbb11f4314ce6e7f3
Dmytro Shevchenko <0911dimon@gmail.com>
2023-08-16 22:44:12 +0300

    title, title style update

--- src/components/home/Title.tsx
+++ src/components/home/Title.tsx
@@ -7,7 +7,13 @@ const Title: React.FC<TitleProps> = () => {
     <section className='title'>
         <div className="container">
             <div className="content">
-                
+                <div className="title-box">
+                    <h1>electric bicycles</h1>
+                    <p>
+                        The Cento10 Hybrid is an electric pedal assist race bike that sets a new, very high standard in the category.
+                    </p>
+                    <button className="standart-btn">Read more...</button>
+                </div>
             </div>
         </div>
     </section>
