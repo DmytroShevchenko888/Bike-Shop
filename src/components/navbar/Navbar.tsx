f4d2d47cbe1323cc6c3dbc1a2e3268e836b28ad1
Dmytro Shevchenko <0911dimon@gmail.com>
2023-08-25 21:40:25 +0300

    Navbar update

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,60 +1,65 @@
-import React, { useState } from 'react'
-import {CiSearch,CiUser,CiHeart,CiShoppingCart,CiMenuFries} from "react-icons/ci"
+import React, { useState } from "react";
+import {
+  CiSearch,
+  CiUser,
+  CiHeart,
+  CiShoppingCart,
+  CiMenuFries,
+} from "react-icons/ci";
 import { getImageUrl } from "../../helpers/getImageUrl";
 import Sidebar from "./Sidebar";
- 
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../constants";
+ROUTES;
+
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
-    const [openNav, setOpenNav] = useState<boolean>(false);
+  const [openNav, setOpenNav] = useState<boolean>(false);
 
-    const closeNav = () => {
-        setOpenNav(!openNav);
-      };
+  const closeNav = () => {
+    setOpenNav(!openNav);
+  };
 
-    return (
-    <header className='navbar'>
-        <Sidebar openNav={openNav} closeNav={closeNav}/>
-        <div className="container">
-            <div className="content">
-            <div className="bike-logo">
+  return (
+    <header className="navbar">
+      <Link to={ROUTES.HOME}>
+        <Sidebar openNav={openNav} closeNav={closeNav} />
+      </Link>
+      <div className="container">
+        <div className="content">
+          <div className="bike-logo">
             <img src={getImageUrl("home", "bikelogo.svg")} alt="logo" />
-        </div>
-        <ul className="nav-menu">
-            <li>
-                Trade in
-            </li>
-            <li>
-                bicycles
-            </li>
-            <li>
-                bicycles
-            </li>
-            <li>
-                parts
-            </li>
-            <li>
-                equipment
-            </li>
-            <li>
-                accessories
-            </li>
-            <li>
-                bike trainer
-            </li>
-        </ul>
-        <div className="user-menu">
-            <div className="search"><CiSearch/></div>
-            <div className="profile"><CiUser/></div>
-            <div className="favorite"><CiHeart/></div>
-            <div className="shopping-cart"><CiShoppingCart/></div>
-            <div className="burger-menu" onClick={closeNav} >
-                <CiMenuFries/>
+          </div>
+          <ul className="nav-menu">
+            <li>Trade in</li>
+            <li>bicycles</li>
+            <li>parts</li>
+            <li>equipment</li>
+            <li>accessories</li>
+            <li>bike trainer</li>
+          </ul>
+          <div className="user-menu">
+            <div className="search">
+              <CiSearch />
             </div>
-        </div>
+            <div className="profile">
+              <CiUser />
             </div>
+            <div className="favorite">
+              <CiHeart />
+            </div>
+            <div className="shopping-cart">
+              <CiShoppingCart />
+            </div>
+            <div className="burger-menu" onClick={closeNav}>
+              <CiMenuFries />
+            </div>
+          </div>
         </div>
+      </div>
     </header>
-)};
+  );
+};
 
 export default Navbar;
