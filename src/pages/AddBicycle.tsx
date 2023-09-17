5c8182f28b84bf17c784752aaa642538cf36341a
Alexey Sevastynov <alexseva94@gmail.com>
2023-09-17 22:33:31 +0300

    fixed page styles "addBicycle"

--- src/pages/AddBicycle.tsx
+++ src/pages/AddBicycle.tsx
@@ -6,6 +6,8 @@ interface AddBicycleProps {}
 const AddBicycle: React.FC<AddBicycleProps> = () => {
   return (
     <section className="add-bicycle">
+      {/*add-bicycle__header ? heigth: 105px; background-color: $bt-black; */}
+      <div className="add-bicycle__header" />
       <div className="container">
         <h3 className="add-bicycle__title">Add New Bicycle:</h3>
 
